61351b0736fd8dd593c2e850425f111565b54af7
Kevin Cordano <kmcordano@gmail.com>
2024-05-26 12:19:39 -0500

    Added Timer and Mine tracker

--- Base/script.js
+++ Base/script.js
@@ -1,54 +1,69 @@
-const numRows = 9;
-const numCols = 9;
+const NUM_ROWS = 9;
+const NUM_COLS = 9;
+const NUM_MINES = 10;
 
-const grid = new Array(numRows);
-for(let i = 0; i < numRows; i++) {
-   grid[i] = new Array(numCols).fill(0);
+const $timer = document.querySelector('.timer');
+const $mineCount = document.querySelector('.mine-count');
+
+var time = 0;
+var mineTracker = NUM_MINES;
+
+const grid = new Array(NUM_ROWS);
+for(let i = 0; i < NUM_ROWS; i++) {
+   grid[i] = new Array(NUM_COLS).fill(0);
 }
 
 var row, col;
-for(let i = 0; i < 10; i++) {
-   row = getRandomInt(numRows);
-   col = getRandomInt(numCols);
+for(let i = 0; i < NUM_MINES; i++) {
+   row = getRandomInt(NUM_ROWS);
+   col = getRandomInt(NUM_COLS);
 
    grid[row][col] = -1;
    incrementNeighbors(row, col);
 }
+
+$mineCount.innerHTML = NUM_MINES;
+$timer.innerHTML = time;
+
+setInterval(() => {
+   time++;
+   $timer.innerHTML = time;
+}, 1000);
 
 function getRandomInt(max) {
    return Math.floor(Math.random() * max);
 };
 
 function incrementNeighbors(row, col) {
-   if(row > 0 && grid[row-1][col] != -1) {
+   if(checkBounds(row-1, col) && grid[row-1][col] != -1) {
       grid[row-1][col]++;
    }
 
-   if(row > 0 && col < numCols - 1 && grid[row-1][col-1] != -1) {
+   if(checkBounds(row-1, col+1) && grid[row-1][col+1] != -1) {
       grid[row-1][col+1]++;
    }
 
-   if(col < numCols - 1 && grid[row][col+1] != -1) {
+   if(checkBounds(row, col+1) && grid[row][col+1] != -1) {
       grid[row][col+1]++;
    }
 
-   if(row < numRows - 1 && col < numCols - 1 && grid[row+1][col+1] != -1) {
+   if(checkBounds(row+1, col+1) && grid[row+1][col+1] != -1) {
       grid[row+1][col+1]++;
    }
 
-   if(row < numRows - 1 && grid[row+1][col] != -1) {
+   if(checkBounds(row+1, col) && grid[row+1][col] != -1) {
       grid[row+1][col]++;
    }
 
-   if(row < numRows - 1 && col > 0 && grid[row+1][col-1] != -1) {
+   if(checkBounds(row+1, col-1) && grid[row+1][col-1] != -1) {
       grid[row+1][col-1]++;
    }
 
-   if(col > 0 && grid[row][col-1] != -1) {
+   if(checkBounds(row, col-1) && grid[row][col-1] != -1) {
       grid[row][col-1]++;
    }
 
-   if(row > 0 && col > 0 && grid[row-1][col-1] != -1) {
+   if(checkBounds(row-1, col-1) && grid[row-1][col-1] != -1) {
       grid[row-1][col-1]++;
    }
 };
@@ -59,11 +74,11 @@ function uncover(id) {
    var numMines;
    var space;
 
-   queue.push([Math.floor(id / numCols), id % numCols]);
+   queue.push([Math.floor(id / NUM_COLS), id % NUM_COLS]);
    while(queue.length > 0) {
       at = queue.shift();
       numMines = grid[at[0]][at[1]];
-      space = document.getElementById((at[0] * numCols) + at[1]);
+      space = document.getElementById((at[0] * NUM_COLS) + at[1]);
 
       if(space.classList.contains('uncovered')
       || space.classList.contains('flag')) {
@@ -108,17 +123,41 @@ function uncover(id) {
    }
 }
 
+function incrementMines() {
+   if(mineTracker >= NUM_MINES) {
+      return;
+   }
+   mineTracker++;
+   $mineCount.innerHTML = mineTracker;
+}
+
+function decrementMines() {
+   if(mineTracker <= 0) {
+      return;
+   }
+   mineTracker--;
+   $mineCount.innerHTML = mineTracker;
+}
+
 function toggleFlag(id) {
    let space = document.getElementById(id);
    if(!space.classList.contains('uncovered')) {
-      space.classList.toggle('flag');
+      // space.classList.toggle('flag');
+      if(space.classList.contains('flag')) {
+         space.classList.remove('flag');
+         incrementMines();
+      }
+      else {
+         space.classList.add('flag');
+         decrementMines();
+      }
    }
 }
 
 function checkBounds(row, col) {
-   return (row < numRows) 
+   return (row < NUM_ROWS) 
        && (row >= 0)
-       && (col < numCols)
+       && (col < NUM_COLS)
        && (col >= 0);
 }
 
